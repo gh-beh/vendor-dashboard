949da99b689262ae910c646f1f4f7cef40bb15bc
ghbeh <bestgreenhorse@gmail.com>
2022-03-29 21:40:39 +0800

    Revert to hash routing

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -12,11 +12,6 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
 
-    // const currentUser = {
-    //     token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJndWlkIjoiYjlmMTVhMGQtMzAzO` +
-        //     `S00YzY0LWI2ZDAtNDQzNzcyZjlkNWRiIn0.gAnx9j16JCvtS0ieIONtAenIQONTR77nN60Xz_6CL8g',
-    // };
-
     const currentUser = this.authenticationService.currentUserValue;
     console.log(currentUser);
     if (currentUser && currentUser.token) {
